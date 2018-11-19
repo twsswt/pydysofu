d647dd78d0d6cb91619af4e94bae2ee0c3e48239
Tom Wallis <github@tomwallis.net>
2018-11-19 13:23:34 +0000

    Slight changes to what's exposed from the package

--- pydysofu/__init__.py
+++ pydysofu/__init__.py
@@ -5,4 +5,4 @@ Front end API for the fuzzi_moss library.
 from .fuzz_decorator import fuzz
 from .fuzz_weaver import fuzz_clazz, defuzz_class, fuzz_module, defuzz_all_classes
 from .config import pydysofu_random
-from .core_fuzzers import fuzzer_invocations, fuzzer_invocations_count, reset_invocation_counters
+from .core_fuzzers import fuzzer_invocations, fuzzer_invocations_count, reset_invocation_counters, remove_last_step, remove_random_step, duplicate_last_step
